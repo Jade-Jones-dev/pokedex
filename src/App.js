26999021f9468b73e2ee4d8fe028ca089b147b71
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-11-12 15:21:43 +0000

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo/>
+      <BestPokemon/>
+      <CaughtPokemon/>
     </div>
+
   );
 }
+
+function Logo(){
+  return (
+    <header>
+        <h1>Welcome to the pokedex</h1>;
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" />
+    </header>
+  );
+}
+
+
+function BestPokemon(){
+  const abilities = ["anticipation", "adapabiliity"];
+  return (
+		<div>
+			<p> My favourite Pokemon is Squirtle</p>
+      <ul>
+        {abilities.map((ability) => (<li>{ability}</li>))}
+        </ul>
+		</div>
+	);
+}
+
+const CaughtPokemon = () => {
+  const date = new Date().toLocaleDateString();
+  return <p> Caught 0 Pokemon on {date}</p>;
+};
 
 export default App;
